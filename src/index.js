121a4bf59d82b34f0196814bb20e110ba0e3f3c4
Kevin <84540535+luminerrr@users.noreply.github.com>
2022-07-18 01:11:49 +0700

    added social media icons

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import Navbar from './components/navbar/Navbar'
 import reportWebVitals from './reportWebVitals';
 import Mainview from './components/mainview/Mainview';
+import Social from './components/social/Social';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
     <Mainview />
+    <Social />
   </React.StrictMode>
 );
 
