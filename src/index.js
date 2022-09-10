219292eb19f8d9162f38ffb066641d4961c8794e
Kevin <tkevinrocket@gmail.com>
2022-09-10 18:27:18 +0700

    added project section

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import Navbar from './components/navbar/Navbar'
 import Mainview from './components/mainview/Mainview';
 import Social from './components/social/Social';
+import Projects from './components/projects/Projects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
     <Mainview />
+    <Projects />
     <Social />
   </React.StrictMode>
 );
