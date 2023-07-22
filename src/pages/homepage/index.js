52ad42a8d000b9e29a6ba5b65feace69f2db2fe5
Kevin <tkevinrocket@gmail.com>
2023-07-22 20:39:46 +0700

    removed unnecessary import

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,4 +1,4 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, } from "@chakra-ui/react";
 import UserLayout from "../../components/UserLayout";
 
 export default function Homepage(){
